293c918cd85d8ffd362a1b10b9d3261ac5697f34
Prajwal Pandgale <pandgale.prajwal11@gmail.com>
2024-08-07 16:43:38 +0530

    Updated Navbar Component into Homepage

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import HomePage from "./Components/Homepage";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Nav from "./Components/Navbar";
+import Homepage from "./Components/Homepage";
 import About from "./Components/About";
-import Contact from "./Components/Contact"; // Import the Contact component
+import Contact from "./Components/Contact";
+import Projects from "./Components/Projects";
 
-function App() {
+const App = () => {
   return (
     <Router>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} /> {/* Add Contact route */}
-        </Routes>
-      </div>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/About" element={<About />} />
+        <Route path="/Contact" element={<Contact />} />
+        <Route path="/Projects" element={<Projects />} />
+      </Routes>
     </Router>
   );
-}
+};
 
 export default App;
